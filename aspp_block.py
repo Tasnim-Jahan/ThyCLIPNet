bc972f6e18f82f61ca90a03088f97d5000dec9ba
Tasnim Jahan <tasnimjahan.cou@gmail.com>
2026-01-08 18:33:44 +0600

    Update aspp_block.py

--- aspp_block.py
+++ aspp_block.py
@@ -12,7 +12,9 @@ def aspp_block(inputs, num_filters=256):
 
     conv3_3 = Conv2D(num_filters, (3, 3), dilation_rate=18, padding="same", activation="relu")(inputs)
 
-    avg_pool = GlobalAveragePooling2D()(inputs)
+    conv3_4 = Conv2D(num_filters, (3, 3), dilation_rate=18, padding="same", activation="relu")(inputs)
+
+    avg_pool = GlobalMaxPooling2D()(inputs)
 
     avg_pool = Reshape((1, 1, inputs.shape[-1]))(avg_pool)
 
@@ -20,6 +22,6 @@ def aspp_block(inputs, num_filters=256):
 
     avg_pool = UpSampling2D(size=(inputs.shape[1], inputs.shape[2]), interpolation="bilinear")(avg_pool)
 
-    output = Concatenate()([conv1, conv3_1, conv3_2, conv3_3, avg_pool])
+    output = Concatenate()([conv1, conv3_1, conv3_2, conv3_3, conv3_4, avg_pool])
 
     return Conv2D(num_filters, (1, 1), padding="same", activation="relu")(output)
